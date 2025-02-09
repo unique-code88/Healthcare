3150ce4a9f0ab456bdaa8d6e0435fef7f3659aaa
Chandanass2003 <chandanassgdg@gmail.com>
2025-02-09 10:34:26 +0530

    first commit

---
+++ vitalcare/website/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,'home.html')
+def about(request):
+    return render(request,'about.html')
+def services(request):
+    return render(request,'services.html')
+def contact(request):
+    return render(request,'contact.html')
+def login(request):
+    return render(request,'login.html')
+def register(request):
+    return render(request,'register.html')
